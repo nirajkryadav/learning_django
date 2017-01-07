4c5bba41d18200b62ed4eeb49f4283863578d616
nkyadav4598 <nirajkumar4598@gmail.com>
2017-01-07 22:18:46 +0530

    first django app part 1 completed yipeee

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b-#61c^mzgx5#nie-l()j*f%n!c7#+8!b4pfdm2qc550k^r#z)'
+SECRET_KEY = 'rsz$%3=lhqt=%v3-c8cdd=kvqz+_&9ot%jrkvst#aka=h8wrxf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
